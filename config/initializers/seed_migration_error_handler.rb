03f78ca9571f6068d73408f297786715ffaecdb1
sameera207 <sameera207@gmail.com>
2018-08-10 11:50:53 +1000

    down method updated

--- config/initializers/seed_migration_error_handler.rb
+++ config/initializers/seed_migration_error_handler.rb
@@ -1,10 +1,18 @@
 module SeedMigration
   class Migrator
     original_up = instance_method(:up)
+    original_down = instance_method(:down)
 
     define_method(:up) do
       begin
         original_up.bind(self).()
+      rescue
+      end
+    end
+
+    define_method(:down) do
+      begin
+        original_down.bind(self).()
       rescue
       end
     end
